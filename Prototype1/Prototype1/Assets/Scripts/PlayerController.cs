b4772b05bb96bef0adc578f62f1f4d227d62732d
tanczos-prodigy <schaeffers@roverkids.org>
2026-04-13 14:16:37 -0400

    finished 1.3

--- Prototype1/Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Prototype1/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }     
