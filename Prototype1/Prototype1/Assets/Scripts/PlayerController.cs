49e6050c445bd53b4897754ad07a47cf1f7f74b2
tanczos-prodigy <schaeffers@roverkids.org>
2026-04-13 13:56:43 -0400

    finished 1.2

--- Prototype1/Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Prototype1/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,13 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //we'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
-}
+}     
