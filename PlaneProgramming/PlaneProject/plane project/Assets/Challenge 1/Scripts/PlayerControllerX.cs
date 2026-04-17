fb425e9abb855d8b6ba9c7e3ccc695426a91c09a
tanczos-prodigy <schaeffers@roverkids.org>
2026-04-17 19:04:40 -0400

    Challenge project 1/2 done

---
+++ PlaneProgramming/PlaneProject/plane project/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerX : MonoBehaviour 
+{
+    public float rotationSpeed;
+    public Slider mySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float speed = mySlider.value;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(x, 0, z);
+
+        // move the plane forward at a constant rate
+        transform.position += move * speed * Time.deltaTime;
+       
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
